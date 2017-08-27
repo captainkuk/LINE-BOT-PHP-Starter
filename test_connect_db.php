88e0e4b5284458b85260226b114b91d28e382167
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 11:53:57 +0700

    Update test_connect_db.php

--- test_connect_db.php
+++ test_connect_db.php
@@ -4,7 +4,12 @@
 </head>
 <body>
 <?php
-include "condb_mysql.php";
+
+$host = "us-cdbr-iron-east-05.cleardb.net";
+$username = "b0188175a00d8f";
+$password = "6c89afbb";
+mysql_connect($host,$username,$password);
+	
 if($objConnect)
 {
 	echo "MySQL Connected";
