6cd8d4104610da46418a5fc7956245304f2dae85
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 13:51:03 +0700

    Update connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
+
 $host = "us-cdbr-iron-east-05.cleardb.net";
 $username = "b0188175a00d8f";
 $password = "6c89afbb";
 $dbname = "heroku_285669661138a8d";
-*/
 
+/*
 $host = "35.194.222.91";
 $username = "root";
 $password = "iamcaptainkuk";
 $dbname = "captain_bot_db1";
+*/
 
 $con = mysqli_connect($host,$username,$password,$dbname);
 
