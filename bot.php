4a3e44c198ba5a62ce61a58283581583e10e8ce7
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 16:58:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -86,33 +86,37 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-			$messages = [
-				'type' => 'text',
-				'text' => 'sticker'
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//curl_setopt($ch, CURLOPT_PROXY, $proxy);
-			//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
 			
+			function postMessage($token,$packet,$urlReply){
+				 $dataEncode = json_encode($packet);
+				 $headersOption = array(‘Content-Type: application/json’,’Authorization: Bearer ‘.$token);
+				 $ch = curl_init($urlReply);
+				 curl_setopt($ch,CURLOPT_CUSTOMREQUEST,’POST’);
+				 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+				 curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
+				 curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
+				 curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+				 $result = curl_exec($ch);
+				 curl_close($ch);
+		    }
+			
+			function getSticker($replyToken){
+				 $sticker = array(
+					‘type’ => ‘sticker’,
+					‘packageId’ => ‘4’,
+					‘stickerId’ => ‘300’
+				 );
+				 $packet = array(
+					‘replyToken’ => $replyToken,
+					‘messages’ => array($sticker),
+				 );
+				 return $packet;
+			}
+			
+			$packet = getSticker($item[‘replyToken’]);
+			postMessage($replyToken,$packet,$url);
+ 
 		}
 	}
 }
