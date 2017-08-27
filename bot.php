415c19264d41430e310c8f121d3e7a203b56bd2d
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 20:17:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -86,7 +86,7 @@ if (!is_null($events['events'])) {
 			
 			$messages = [
 				'type' => 'text',
-				'text' => $stickerid
+				'text' => 'sticker'
 			];
 			
 			// Make a POST Request to Messaging API to reply to sender
