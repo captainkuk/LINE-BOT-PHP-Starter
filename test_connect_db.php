e92752c22d319846599f8f52a20004ac06213bbf
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 11:58:57 +0700

    Update test_connect_db.php

--- test_connect_db.php
+++ test_connect_db.php
@@ -1,21 +1,6 @@
 
 <?php
 
-$host = "us-cdbr-iron-east-05.cleardb.net";
-$username = "b0188175a00d8f";
-$password = "6c89afbb";
-mysql_connect($host,$username,$password);
-	
-if($objConnect)
-{
-	echo "MySQL Connected";
-}
-else
-{
-	echo "MySQL Connect Failed : Error : ".mysql_error();
-}
-
-mysql_close($objConnect);
 
 echo "55555";
 ?>
