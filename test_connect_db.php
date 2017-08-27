f436f63c3d15012cd56ca69727cca677d2810ba1
captainkuk <naicaptainkuk@gmail.com>
2017-08-27 12:18:28 +0700

    Update test_connect_db.php

--- test_connect_db.php
+++ test_connect_db.php
@@ -1,7 +1,11 @@
-
 <?php
-
-
-echo "55555";
+include "condb_mysql.php";
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}else{
+	echo "Conect Success! ";
+}
+//mysqli_close($objConnect);
 ?>
-
